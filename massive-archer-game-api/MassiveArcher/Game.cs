3e360d0936f2428ef751ca93d3b62f0201d1393a
gamenew09 <gamenew09@gmail.com>
2012-08-30 20:48:09 -0500

    first commit

---
+++ massive-archer-game-api/MassiveArcher/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassiveArcher
+{
+    abstract class Game : Form
+    {
+
+        public Graphics graphics;
+
+        public Game()
+        {
+            graphics = MassiveArcher.createGraphicsObject(this);
+        }
+
+        public void drawAndUpdate()
+        {
+            Update();
+            Draw();
+        }
+
+        public abstract void Update();
+
+        public abstract void Draw();
+    }
+}
---
+++ massive-archer-game-api/MassiveArcher/MassiveArcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MassiveArcher
+{
+    public class MassiveArcher
+    {
+
+        public static MassiveArcher getInstance()
+        {
+            return new MassiveArcher();
+        }
+
+        public Graphics createGraphicsObject(Game g)
+        {
+            return g.CreateGraphics();
+        }
+
+        public void drawLine(Graphics g, Pen p, Point p1, Point p2)
+        {
+            g.DrawLine(p, p1, p2);
+        }
+
+    }
+}
